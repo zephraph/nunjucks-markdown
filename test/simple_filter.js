4c4707d24393276301c3c1443b2831055267aa76
Gary Green <holegary@gmail.com>
2014-10-24 16:57:19 +0100

    Fixed markdown tag

--- test/simple_filter.js
+++ test/simple_filter.js
@@ -1,15 +1,19 @@
 var nunjucks = require('nunjucks'),
-    expect   = require('chai').expect(),
+    expect   = require('chai').expect,
     filter   = require('../lib/main.js');
 
-var template = '{% markdown %} # Hello! This is markdown! {% endmarkdown %}';
 var env = nunjucks.configure('views');
 
 filter.register(env);
-describe('Filter Block', function() {
-  describe('Single filter: upper', function() {
-    it('should return an uppercase string', function() {
-      env.renderString(template);
-    });
-  });
+
+describe('markdown tag', function() {
+
+	it('should parse basic markdown', function() {
+
+		var tmpl = '{% markdown %} # Hello! This is markdown! {% endmarkdown %}';
+		var result = env.renderString(tmpl);
+
+		return expect(result).to.equal('<h1 id="hello-this-is-markdown-">Hello! This is markdown!</h1>\n');
+	});
+
 });
